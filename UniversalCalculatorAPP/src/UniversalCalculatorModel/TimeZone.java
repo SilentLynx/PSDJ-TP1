d55b9f8088b9c258039d3dd7b176f2d4c0879f83
Daniel Maia <33250763+SilentLynx@users.noreply.github.com>
2018-12-01 12:47:08 +0000

    Pequeno ajuste ao método de cálculo de fuso horário

--- UniversalCalculatorAPP/src/UniversalCalculatorModel/TimeZone.java
+++ UniversalCalculatorAPP/src/UniversalCalculatorModel/TimeZone.java
@@ -15,20 +15,24 @@ import java.util.Date;
 public class TimeZone {
     
     public static String calcular(Date o, LocalTime t, int offsetFrom, int offsetTo, boolean check){
-          OffsetDateTime odt;
+          //OffsetDateTime odt;
           LocalDateTime ldt;
+          
+          ldt = LocalDateTime.ofInstant(o.toInstant(), ZoneId.systemDefault()).with(t);
           
           if(check == true)
           {
-              ldt = LocalDateTime.ofInstant(o.toInstant(), ZoneId.systemDefault()).with(t);  //o.toInstant().atOffset(ZoneId.systemDefault().getRules().getOffset(Instant.now())).with(t);
+              //o.toInstant().atOffset(ZoneId.systemDefault().getRules().getOffset(Instant.now())).with(t);
               ldt = ldt.plusHours(offsetTo - (ZoneId.systemDefault().getRules().getOffset(Instant.now()).getTotalSeconds() / 3600));//odt.withOffsetSameInstant(ZoneOffset.ofHours(offsetTo)).toLocalDateTime();
           }
           else { 
-              ldt = LocalDateTime.ofInstant(o.toInstant(), ZoneId.systemDefault()).with(t);//odt = o.toInstant().atOffset(ZoneOffset.UTC).with(t);
+              //odt = o.toInstant().atOffset(ZoneOffset.UTC).with(t);
                                 //odt = OffsetDateTime.parse(o.toString() + "T" + t.toString() + String.valueOf(offsetFrom));
               ldt = ldt.plusHours(offsetTo - offsetFrom);//ldt = odt.withOffsetSameInstant(ZoneOffset.ofHours(offsetTo)).toLocalDateTime();
           }
           
           return ldt.toString();
     }
+    
+    
 }
